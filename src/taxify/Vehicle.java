9b0ef57dc810c769f83e9faef8ad88e2ba6f389d
Aaron <aaronli5260@gmail.com>
2025-02-27 16:58:40 +0100

    provided

---
+++ src/taxify/Vehicle.java
@@ -0,0 +1,151 @@
+package taxify;
+
+public abstract class Vehicle implements IVehicle {
+    private int id;
+    private ITaxiCompany company;
+    private IService service;
+    private VehicleStatus status;
+    private ILocation location;
+    private ILocation destination;
+    private IStatistics statistics;
+    private IRoute route;
+        
+    public Vehicle(int id, ILocation location) {        
+        this.id = id;
+        this.service = null;
+        this.status = VehicleStatus.FREE;
+        this.location = location;        
+        this.destination = ApplicationLibrary.randomLocation(this.location);
+        this.statistics = new Statistics();
+        this.route = new Route(this.location, this.destination);
+    }
+
+    @Override
+    public int getId() {
+        // return id
+    }
+ 
+    @Override
+    public ILocation getLocation() {
+        // return location
+    }
+
+    @Override
+    public ILocation getDestination() {
+        // return destination
+    }
+    
+    @Override
+    public IService getService() {
+        // return service
+    }
+    
+    @Override
+    public IStatistics getStatistics() {
+        // return statistics
+    }
+    
+    @Override
+    public void setCompany(ITaxiCompany company) {
+        this.company = company;
+    }
+    
+    @Override
+    public void pickService(IService service) {
+        // pick a service, set destination to the service pickup location, and status to "pickup"
+        
+        this.service = service;
+        this.destination = service.getPickupLocation();
+        this.route = new Route(this.location, this.destination);        
+        this.status = VehicleStatus.PICKUP;
+    }
+
+    @Override
+    public void startService() {
+        // set destination to the service drop-off location, and status to "service"
+    }
+
+    @Override
+    public void endService() {
+        // update vehicle statistics
+        
+        this.statistics.updateBilling(this.calculateCost());
+        this.statistics.updateDistance(this.service.calculateDistance());
+        this.statistics.updateServices();
+        
+        // if the service is rated by the user, update statistics
+        
+        if (this.service.getStars() != 0) {
+            this.statistics.updateStars(this.service.getStars());
+            this.statistics.updateReviews();
+        }
+        
+        // set service to null, and status to "free"
+        
+        this.service = null;
+        this.destination = ApplicationLibrary.randomLocation(this.location);
+        this.route = new Route(this.location, this.destination);
+        this.status = VehicleStatus.FREE;
+    }
+
+    @Override
+    public void notifyArrivalAtPickupLocation() {
+        // notify the company that the vehicle is at the pickup location and start the service
+    }
+        
+    @Override
+    public void notifyArrivalAtDropoffLocation() {
+        // notify the company that the vehicle is at the drop off location and end the service
+     }
+        
+    @Override
+    public boolean isFree() {
+        // returns true if the status of the vehicle is "free" and false otherwise
+    }   
+    
+    @Override
+    public void move() {
+        // get the next location from the driving route
+        
+        this.location = this.route.getNextLocation();        
+        
+        // if the route has more locations the vehicle continues its route, otherwise the vehicle has arrived to a pickup or drop off location
+        
+        if (!this.route.hasLocations()) {
+            if (this.service == null) {
+                // the vehicle continues its random route
+
+                this.destination = ApplicationLibrary.randomLocation(this.location);
+                this.route = new Route(this.location, this.destination);
+            }
+            else {
+                // check if the vehicle has arrived to a pickup or drop off location
+
+                ILocation origin = this.service.getPickupLocation();
+                ILocation destination = this.service.getDropoffLocation();
+
+                if (this.location.getX() == origin.getX() && this.location.getY() == origin.getY()) {
+
+                    notifyArrivalAtPickupLocation();
+
+                } else if (this.location.getX() == destination.getX() && this.location.getY() == destination.getY()) {
+
+                    notifyArrivalAtDropoffLocation();
+
+                }        
+            }
+        }
+    }
+
+    @Override
+    public int calculateCost() {
+        return this.service.calculateDistance();
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " at " + this.location + " driving to " + this.destination +
+               ((this.status == VehicleStatus.FREE) ? " is free with path " + this.route.toString(): ((this.status == VehicleStatus.PICKUP) ?
+               " to pickup user " + this.service.getUser().getId() : " in service "));
+    }    
+}
